a9f1402d824795d4374aaa705b684d39e1841078
Claire <clairelogan16@gmail.com>
2021-08-25 18:23:10 -0700

    various updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,20 +5,22 @@ class ApplicationController < Sinatra::Base
 
   get "/posts" do
     #get all posts, starting with the most recent
-    Post.all.order('created_at DESC').to_json(include: {user: {only: [:username]}, community: {only: [:name]}, replies: {include:[user: {only: [:username]}]}, likes: {only: [:post_id, :user_id]}})
+    Post.all.order('created_at DESC').to_json(include: {user: {only: [:username, :avatar]}, community: {only: [:name, :emoji]}, replies: {include:[user: {only: [:username, :avatar]}]}, likes: {only: [:post_id, :user_id]}})
   end
 
-  get "/posts/top" do
-    #get all posts, starting with the most recent
-    Post.all.order('created_at ASC').to_json(include: {user: {only: [:username]}, community: {only: [:name]}, replies: {include:[user: {only: [:username]}]}, likes: {only: [:post_id, :user_id]}})
-  end
+  # get "/posts/top" do
+  #   #get all posts, starting with the most recent
+  #   Post.all.order('created_at ASC').to_json(include: {user: {only: [:username, :avatar]}, community: {only: [:name]}, replies: {include:[user: {only: [:username, :avatar]}]}, likes: {only: [:post_id, :user_id]}})
+  # end
 
   get '/posts/:id' do
+    #get post by ID
     post = Post.find(params[:id])
     post.to_json(include: [:likes, :replies])
   end
 
   get '/posts/:id/likecount' do
+    # special get that calculates the number of likes a give post has
     post_likes = Post.find(params[:id]).like_count
     post_likes.to_json
   end
@@ -44,7 +46,7 @@ class ApplicationController < Sinatra::Base
       content: params[:content], 
       image_url: params[:image_url], 
       user_id: params[:user_id], 
-      community_id: params[:community_id]).to_json(include: {user: {only: [:username]}, community: {only: [:name]}, replies: {include:[user: {only: [:username]}]}, likes: {only: [:post_id, :user_id]}})
+      community_id: params[:community_id]).to_json(include: {user: {only: [:username, :avatar]}, community: {only: [:name, :emoji]}, replies: {include:[user: {only: [:username, :avatar]}]}, likes: {only: [:post_id, :user_id]}})
   end
 
   post '/likes' do
